c3cffc1edc001e7fdb2e3f11b426b5bc8bd6b968
Lucky4604 <luckygovindrao182@gmail.com>
2023-03-29 12:04:16 +0530

    first schema is done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ const express=require("express");
 
 const connect=require("./config/database")
 
+const Tweet=require('./models/tweet');
+
 const app=express();
 
 app.listen(3000, async()=>{
     console.log("server running on port 3000")
     await connect();
     console.log("mongo db connected")
+    const tweet=await Tweet.create({
+        content:"first tweet",
+        userEmail:"luck@gmail.com"
+    });
+    console.log(tweet)
 })
