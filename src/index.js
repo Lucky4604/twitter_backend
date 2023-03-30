6ab9d43485305fff9c4efade13e69a606257a9f9
Lucky4604 <luckygovindrao182@gmail.com>
2023-03-30 17:12:12 +0530

    added codes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express=require("express");
 
-const connect=require("./config/database")
+const connect=require("./config/database");
 
-const Tweet=require('./models/tweet');
+
+const TweetRepository=require('./repository/tweet-repository');
 
 const app=express();
 
@@ -10,9 +11,16 @@ app.listen(3000, async()=>{
     console.log("server running on port 3000")
     await connect();
     console.log("mongo db connected")
-    const tweet=await Tweet.create({
-        content:"first tweet",
-        userEmail:"luck@gmail.com"
-    });
-    console.log(tweet)
+    // const tweet=await Tweet.create({
+    //     content:"second tweet"
+
+    // });
+  const tweetRepo=new TweetRepository();
+  const tweet=await tweetRepo.create({content:'my tweet'})
+  console.log(tweet);
+  tweet.comments.push({content:'first comment '})
+  await tweet.save();
+  console.log(tweet);
+  
+    
 })
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -5,10 +5,21 @@ const tweetSchema=new mongoose.Schema({
         type:String,
         required:true
     },
+    
     userEmail:{
         type:String,
+    },
+    comments:[{
+        content:{
+        type:String,
+        required:true
     }
-});
+
+    
+
+    }
+]
+}, {timestamps:true});
 
 const Tweet=mongoose.model('Tweet',tweetSchema);
 module.exports =Tweet;
